e4cf78158cc82361df3ff8034b0c48bdf6c9ae0a
Luis <luis@escolano.es>
2023-12-12 00:42:25 +0100

    Falta parte opcional

--- Practica 1/server.js
+++ Practica 1/server.js
@@ -1,5 +1,6 @@
 const http = require("http")
 const fs = require("fs")
+const path = require("path")
 
 const read = (err, data) => {
     if (err) {
@@ -9,7 +10,16 @@ const read = (err, data) => {
     return data
 }
 
-const requestListener = function (request, response){
+function getContentType(url){
+    extension = path.extname(url)
+    if ([".png", ".jpg", ".jpeg", ".gif", ".ico"].includes(extension))
+        return "image/" + extension.substring(1);
+    if(extension == ".css")
+        return "text/css"
+    return "text/html"
+}
+
+function printLog(request, response){
     console.log("Se ha producido una petición")
     console.log("URL: " + request.url)
     console.log("Método: " + request.method)
@@ -17,24 +27,32 @@ const requestListener = function (request, response){
     console.log("Host: " + request.headers['host'])
     console.log("User-Agent: " + request.headers['user-agent'])
     console.log("Accept-Language: " + request.headers['accept-language']) 
-    path = request.url
-    if (path == "/")
-        path = "/index.html"
-        console.log(path)
-    fs.readFile("." + path, (err, data) =>{
+}
+
+const requestListener = function (request, response){
+    printLog(request, response)
+
+    p = request.url == "/" ? "/index.html" : request.url
+
+    const contentType = getContentType(p)
+    console.log("Content type: " + contentType)
+    console.log("Path: " + p)
+
+    fs.readFile("." + p, (err, data) =>{
         if (err) {
             response.writeHead(500)
             response.end("Internal server error")
             return
         }
-        response.setHeader("Content-Type", "text/html");
+        
+
+        response.setHeader("Content-Type", contentType);
         response.writeHead(200);
         response.write(data);
         response.end();
-        
-    });
+    }); 
+    console.log("")
 };
-
 
 const server = http.createServer(requestListener);
 server.listen(80);
